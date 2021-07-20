8a4620b22df4755df1cb862019882eb3cc55f3f5
Courtney Frey <cfrey@launchcode.org>
2021-07-20 15:06:54 -0500

    second message added

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -7,6 +7,7 @@ namespace communication_log
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Thisbe!");
+            Console.WriteLine("Hello, Alyce! ");
         }
     }
 }
