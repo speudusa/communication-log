5b1fafc61e53b85d87bdfe74dbc96d1573cd1290
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2021-07-20 15:31:15 -0500

    hello willow

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -7,7 +7,8 @@ namespace communication_log
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Thisbe!");
-            Console.WriteLine("Hello, Alyce! ");
+            Console.WriteLine("Hello, Alyce!");
+            Console.WriteLine("Hello, Willow!");
         }
     }
 }
