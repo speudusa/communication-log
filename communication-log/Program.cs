a538e77276d7711e5cd82ee233fc7ad96ad14249
Courtney Frey <66076696+speudusa@users.noreply.github.com>
2021-07-21 09:27:23 -0500

    howdy partner

--- communication-log/Program.cs
+++ communication-log/Program.cs
@@ -6,17 +6,9 @@ namespace communication_log
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number.");
-            string input = Console.ReadLine();
-
-            if (Int32.Parse(input) % 2 == 0)
-            {
-                Console.WriteLine("Your number is even");
-            }
-            else
-            {
-                Console.WriteLine("Your number is odd.");
-            }
+            Console.WriteLine("Hello, Thisbe!");
+            Console.WriteLine("Hello, Alyce!");
+            Console.WriteLine("Hello, Willow!");
         }
     }
 }
